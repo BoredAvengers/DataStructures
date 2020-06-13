3eb41ccbe5a0cd7574f40469705310bec511a50d
sujay-dsa <30657643+sujay-dsa@users.noreply.github.com>
2020-06-13 13:20:43 +0530

    Code changes for Lambda function handler to accept input from API Gateway and use bracket sequence validator to return the result of validation. Also included open api definition of the api to integrate with lambda.

--- Practice/src/main/java/stack/bracketcheck/LambdaFunctionHandler.java
+++ Practice/src/main/java/stack/bracketcheck/LambdaFunctionHandler.java
@@ -1,26 +1,27 @@
 package stack.bracketcheck;
 
+import java.util.LinkedHashMap;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 
+	@Override
 	public String handleRequest(Object input, Context context) {
 		context.getLogger().log("Input: " + input);
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		LinkedHashMap<String, String> map = (LinkedHashMap<String, String>) input;
 
 		BracketChecker.reversedBrackets.put("(", ")");
 		BracketChecker.reversedBrackets.put("[", "]");
 		BracketChecker.reversedBrackets.put("{", "}");
 
-		JsonElement jsonElement = gson.toJsonTree(input);
+		context.getLogger().log("brackets: " + map.get("brackets"));
+		String brackets = map.get("brackets");
 
-		JsonObject jsonObject = jsonElement.getAsJsonObject();
-		return BracketChecker.validateBracketSequence(jsonObject.get("brackets").getAsString());
+		String result = BracketChecker.validateBracketSequence(brackets);
+
+		return result;
 	}
 
 }
